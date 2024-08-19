09c75f0b564b45bed2b6197bf0767fea9fa1b1a7
ahmed-khalil-96 <ahmed.elabassiry96@gmail.com>
2024-08-19 04:02:23 +0300

    update stripe

--- src/initApp.js
+++ src/initApp.js
@@ -11,43 +11,41 @@ import path from "path"
 import cors from "cors"
 import Stripe from 'stripe';
 import { asyncHandler } from './utils/errorHandling.js'
-import { adminApplication } from './Modules/User/user.controller.js'
 let  checkoutSessionCompleted
 const stripe = new Stripe(process.env.stripe_secret);
 
 
 export const initApp = (app, express)=>{
-    app.use(express.json({
-        verify: function (req, res, buf) {
-            var url = req.originalUrl;
-            if (url.startsWith('/webhook')) {
-                req.rawBody = buf.toString()
-            }
+    app.use((req, res, next) => {
+        if (req.originalUrl === '/webhook') {
+        next();
+        } else {
+        express.json()(req, res, next);
         }
-    }));
-      
-      // Stripe requires the raw body to construct the event
-      app.post('/webhook', express.raw({type: 'application/json'}), (req, res) => {
+        });
+    
+  
+    app.post('/webhook', express.raw({type:'application/json'}), (req, res) => {
         const sig = req.headers['stripe-signature'];
       
         let event;
-      
         try {
-          event = stripe.webhooks.constructEvent(req.rawBody, sig, process.env.endpointSecret);
+          event = stripe.webhooks.constructEvent(req.body, sig, process.env.endpointSecret);
         } catch (err) {
-          // On error, log and return the error message
-          console.log(`❌ Error message: ${err.message}`);
-          return res.status(400).send(`Webhook Error: ${err.message}`);
+          res.status(400).send(`Webhook Error: ${err.message}`);
+          return;
+        }
+       
+        if (event.type ==="checkout.session.completed") {
+        
+             checkoutSessionCompleted = event.data.object;
         }
       
-        // Successfully constructed event
-        console.log('✅ Success:', event.id);
-      
-        // Return a response to acknowledge receipt of the event
-        res.json({received: true});
+        res.status(200).json({msg:"done"},checkoutSessionCompleted);
       });
+      
 
-app.use(express.json())
+
 app.use(cors())
 connection()
 app.get("/",(req,res)=>{
